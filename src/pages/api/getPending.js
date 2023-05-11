fcdfef661c02229df24f4f5d253b84455a4f25ac
davidtey <davidteyby@gmail.com>
2023-05-11 16:31:00 +0800

    .

---
+++ src/pages/api/getPending.js
@@ -0,0 +1,31 @@
+import '../../clientConfig'
+import { getPending } from '../../clientConfig'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('request: ', req.body)
+    // Perform any necessary validation or processing of the form data here
+    const data = JSON.parse(req.body)
+    
+    const {userID} = data
+    
+    console.log(userID)
+
+    const pendingChanges = await getPending(userID);
+
+    // Check if data is missing
+    console.log(pendingChanges)
+
+    if(pendingChanges.data.length === 0) {
+        res.status(200).json({ message: `MISSING_CONFIG`});
+    } else {
+        // If Data Is Present
+        
+    }
+    console.log("pending changes", pendingChanges)    
+
+  } else {
+    // Handle invalid request methods
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
