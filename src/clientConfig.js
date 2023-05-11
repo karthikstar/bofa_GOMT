476a58950f11b46dd9e393913c7e709a9794866c
davidtey <davidteyby@gmail.com>
2023-05-11 12:26:41 +0800

    update

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -3,36 +3,28 @@ import { Database } from 'lib/database.types'
 
 const supabase = createClient('https://djqvepfyrsuttgfydjkf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcXZlcGZ5cnN1dHRnZnlkamtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3Njg4MDgsImV4cCI6MTk5OTM0NDgwOH0.3qhWNz0u2c_1pzL6dfm3NsxKk7VEGT9RJJJ1o1zlrzs')
 
-async function getConfig(username){
-    clientID = supabase.from('Users').select('id').eq('username', username)
-
+async function getConfig(clientID){
     return await supabase.from('current_info')
         .select('commission_difference', 'grossamount_difference')
-        .eq('id', clientID)
+        .eq('client_id', clientID)
         .eq('active_status', true)
 }
 
-async function getPending(){
+async function getPending(userID){
     return await supabase.from('current_info')
-        .select('id, user_id, commission_difference, grossamount_difference')
-        .eq('approval_status', false)
+        .select('id, client_id, commission_difference, grossamount_difference')
+        .eq('approval_status', 'PENDING')
+        .neq('user_id', userID)
 }
 
-async function getVersions(username){
-    clientID = supabase.from('Users').select('id').eq('username', username)
-
-    archiveIDs = supabase.from('current_info')
-        .select('id')
-        .eq('user_id', clientID)
-
+async function getVersions(clientID){
     return await supabase.from('archive_info')
-        .select('time, snapshot, edit_comments')
-        .eq('id', archiveIDs)
+        .select('id, client_id, time, snapshot, edit_comments')
+        .eq('client_id', clientID)
 }
 
-async function approve(client, configID, approver){
+async function approve(clientID, configID, username, editComments){
     approverID = supabase.from('Users').select('id').eq('username', username)
-    clientID = supabase.from('Users').select('id').eq('username', client)
 
     supabase.from('current_info')
         .update({active_status: false})
@@ -40,18 +32,29 @@ async function approve(client, configID, approver){
         .eq('active_status', true)
 
     supabase.from('current_info')
-        .update({approval_status: true, approver_id: approverID, active_status: true})
+        .update({approval_status: "APPROVED", approver_id: approverID, active_status: true})
         .eq('id', configID)
     
     currentSnapshot = snapshot(configID)
 
     supabase.from('archive_info')
-        .insert({snapshot: currentSnapshot})
+        .insert({snapshot: currentSnapshot, edit_comments: editComments, client_id: clientID})
     
 }
 
-async function rollback(){
+async function rollback(archiveID){
+    currentSnapshot = supabase.from('archive_info')
+        .select('snapshot')
+        .eq('id', archiveID)
+
     
+    supabase.from('current_info')
+        .update({active_status: false})
+        .eq('id', clientID) //clientID get from snapshot
+        .eq('active_status', true)
+
+    supabase.from('current_info')
+        .insert({approver_id: , grossamount_difference: , commission_difference: , approval_status: , active_status: true, time: , client_id: , user_id: }) //insert columns 
 }
 
 async function snapshot(configID){
