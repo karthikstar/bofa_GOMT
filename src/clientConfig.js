557f14393f8c3da2ef2f303247858872898176a3
davidtey <davidteyby@gmail.com>
2023-05-11 13:24:36 +0800

    .

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -3,6 +3,14 @@ import { Database } from 'lib/database.types'
 
 const supabase = createClient('https://djqvepfyrsuttgfydjkf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcXZlcGZ5cnN1dHRnZnlkamtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3Njg4MDgsImV4cCI6MTk5OTM0NDgwOH0.3qhWNz0u2c_1pzL6dfm3NsxKk7VEGT9RJJJ1o1zlrzs')
 
+function login(username, password){
+    target = supabase.from('Users')
+        .select('password')
+        .eq('username', username)
+    
+    return password == target
+}
+
 async function getConfig(clientID){
     return await supabase.from('current_info')
         .select('commission_difference', 'grossamount_difference')
@@ -23,9 +31,13 @@ async function getVersions(clientID){
         .eq('client_id', clientID)
 }
 
-async function approve(clientID, configID, username, editComments){
-    approverID = supabase.from('Users').select('id').eq('username', username)
+async function submit(clientID, grossAmountDifference, commissionDifference, userID){
+    supabase.from('current_info')
+    .insert({grossamount_difference: grossAmountDifference, commission_difference: commissionDifference, approval_status: "PENDING", 
+        active_status: false, time: Date.now(), client_id: clientID, user_id: userID})
+}
 
+async function approve(clientID, configID, approverID, editComments){
     supabase.from('current_info')
         .update({active_status: false})
         .eq('id', clientID)
@@ -54,7 +66,9 @@ async function rollback(archiveID){
         .eq('active_status', true)
 
     supabase.from('current_info')
-        .insert({approver_id: , grossamount_difference: , commission_difference: , approval_status: , active_status: true, time: , client_id: , user_id: }) //insert columns 
+        .insert({approver_id: currentSnapshot.approver_id, grossamount_difference: currentSnapshot.grossamount_difference, 
+            commission_difference: currentSnapshot.commission_difference, approval_status: currentSnapshot.approval_status, 
+            active_status: true, time: currentSnapshot.time, client_id: currentSnapshot.client_id, user_id: currentSnapshot.user_id}) //insert columns 
 }
 
 async function snapshot(configID){
@@ -64,3 +78,4 @@ async function snapshot(configID){
 
     return currentSnapshot //check if json format
 }
+
